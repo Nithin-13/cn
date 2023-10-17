e7e02e02f565348c5f77e2e83315241874cc41b2
labcsm <141944315+labcsm@users.noreply.github.com>
2023-10-17 14:38:03 +0530

    Update crc.c

--- crc.c
+++ crc.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<string.h>
-
+char msg[100],g[10],x1[10],rem[10],rcv[100],urcv[100];
+int m,n;
+int j=0,f=-1,k=0;
 char xor(char a,char b)
 {
 	if(a==b)
@@ -9,68 +11,108 @@ char xor(char a,char b)
 	return '1';
 }
 
-
+void cal(char x2[])
+{
+  	k=0;
+		for(int i=0;i<n;i++)
+		{
+			rem[i]=xor(x1[i],g[i]);
+		}
+		while(rem[k]!='1' && k<=n)
+		{
+			k++;
+		}
+		
+		
+		int q=0,p=k;
+		
+		
+		while(rem[p]!='\0')
+		{
+			x1[q++]=rem[p++];
+		}
+		
+		for(int r=0;r<k;r++)
+		{
+			x1[q++]=x2[j++];
+		}
+}
 void main()
 {
-	char msg[100],g[10],x1[10],rem[10],rcv[100];
-	int m,n;
 	printf("Enter the message : ");
 	scanf("%s",msg);
-	int i=0;
 	printf("\nEnter Divisor : ");
 	scanf("%s",g);
 	n=strlen(g);
 	m=strlen(msg);
+	
+	for(int i=0;i<m;i++)
+	{
+		rcv[i]=msg[i];
+	}
+	
 	for(int i=m;i<m+n-1;i++)
 	{
 		msg[i]='0';
 	}
-	printf(" After appending zeroes the message : %s",msg);
 	
-	int j=0,f=-1,k=0;
+	printf("\nAfter appending zeroes the message : %s\n",msg);
+	
 	for(int i=0;i<n;i++)
 	{
 			x1[i]=msg[j++];
 	}
-	int c=0;
+	
 	while(msg[j]!='\0')
 	{
-		k=0;
-		for(int i=0;i<n;i++)
-		{
-			rem[i]=xor(x1[i],g[i]);
-		}
-		while(rem[k]!='1')
-		{
-			k++;
-		}
-		int q=0,p=k;
-		while(rem[p]!='\0')
-		{
-			x1[q++]=rem[p++];
-		}
-		for(int r=0;r<k;r++)
-		{
-			x1[q++]=msg[j++];
-		}
-		printf("\nResult : %s\n X1 : %s\n Iter %d\n K val : %d\n",rem,x1,c,k);
-		c++;
+		cal(msg);
 	}
-		k=0;
-		for(int i=0;i<n;i++)
+	
+	
+	cal(msg);
+	
+	printf("\nReminder  is  : %s\n",rem);
+	
+	int ind=1;
+	for(int i=m;i<m+n-1;i++)
+	{
+		rcv[i]=rem[ind++];
+	}
+	
+	printf("\nReciever message without errors is : %s\n",rcv);
+	
+	printf("\nEnter the Recieved message : ");
+	scanf("%s",urcv);
+	
+	
+	j=0;
+	for(int i=0;i<n;i++)
+	{
+			x1[i]=urcv[j++];
+	}
+	
+	while(urcv[j]!='\0')
+	{
+		cal(urcv);
+	}
+	cal(urcv);
+	
+	
+	printf("\nReminder of given msg is : %s\n",rem);
+	
+	int flag=1;
+	
+	for(int i=0;i<n;i++)
+	{
+		if(rem[i]!='0')
 		{
-			rem[i]=xor(x1[i],g[i]);
+			flag=0;
 		}
-		while(rem[k]!='1')
-		{
-			k++;
-		}
-		int q=0,p=k;
-		while(rem[p]!='\0')
-		{
-			x1[q++]=rem[p++];
-		}
-		printf("\nReminder : %s",rem);		
+	}
+	if(flag)
+		printf("\nNO ERROR ! \n\n");
+	else
+		printf("\n ERROR DETECTED!!!!\n\n");	
 }
 	
 	 	
